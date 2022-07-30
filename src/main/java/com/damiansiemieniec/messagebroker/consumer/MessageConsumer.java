b68c5e324a13642aae03ab46bf528aa76e39b1cf
Damian Siemieniec <damian.siemieniec@gmail.com>
2022-07-30 11:52:52 +0200

    Send logs to apache solr

--- src/main/java/com/damiansiemieniec/messagebroker/consumer/MessageConsumer.java
+++ src/main/java/com/damiansiemieniec/messagebroker/consumer/MessageConsumer.java
@@ -1,5 +1,9 @@
 package com.damiansiemieniec.messagebroker.consumer;
 
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.apache.solr.client.solrj.response.UpdateResponse;
+import org.apache.solr.common.SolrInputDocument;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
@@ -8,12 +12,16 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.sql.Time;
+import java.util.UUID;
 
 @Component
 public class MessageConsumer {
     @JmsListener(destination = "message_broker")
     public void processMessage(String content) {
-        System.out.println("Received message: " + content);
+        UUID uuid = UUID.randomUUID();
+
+        indexMessage("Received message: " + content, uuid);
 
         var client = HttpClient.newHttpClient();
         var uri = URI.create("http://localhost:8000/castlemock/mock/rest/project/szSWBv/application/GwHaVy/some-funny-endpoint");
@@ -23,8 +31,31 @@ public class MessageConsumer {
 
         try {
             var response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            System.out.println("Subscriber response: " +  response.body());
+            indexMessage("Subscriber response: " +  response.body(), uuid);
         } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            indexMessage("Exception: " + e.getMessage(), uuid);
+        }
+    }
+
+    private void indexMessage(String message, UUID uuid) {
+        System.out.println(uuid.toString() +  " " + message);
+
+        final String solrUrl = "http://localhost:8983/solr";
+        var client = new HttpSolrClient.Builder(solrUrl)
+                .withConnectionTimeout(10000)
+                .withSocketTimeout(60000)
+                .build();
+
+        final SolrInputDocument doc = new SolrInputDocument();
+        doc.addField("event_id", uuid.toString());
+        doc.addField("message", message);
+        doc.addField("timestamp", System.currentTimeMillis());
+
+        try {
+            final UpdateResponse updateResponse = client.add("gettingstarted", doc);
+            client.commit("gettingstarted");
+        } catch (SolrServerException | IOException e) {
             e.printStackTrace();
         }
     }
